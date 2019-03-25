6cea3afebaf87c1edc9492dd6c96b9e12d4ef048
morninng <mr_morninng_star_dash@yahoo.co.jp>
2019-03-25 19:13:14 +0900

    update data on firebase by drag and drop

--- src/app/drag-drop-firebase/drag-drop-firebase.component.ts
+++ src/app/drag-drop-firebase/drag-drop-firebase.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { map } from 'rxjs/Operators';
 import { AngularFirestore } from '@angular/fire/firestore';
 
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
@@ -8,6 +8,9 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 
 interface User {
   name: string,
+}
+interface UserWithId extends User {
+  id?: string,
 }
 
 @Component({
@@ -17,8 +20,9 @@ interface User {
 })
 export class DragDropFirebaseComponent implements OnInit {
 
-  itemsA: User[];
-  itemsB: User[];
+  itemsA: UserWithId[];
+  itemsB: UserWithId[];
+  itemsC: UserWithId[];
 
   constructor(
     private db: AngularFirestore,
@@ -26,38 +30,99 @@ export class DragDropFirebaseComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.db.collection<User>('items-a').valueChanges().subscribe((data: User[])=>{
+    this.db.collection<User>('items-a').snapshotChanges()
+    .pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as User;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    )
+    .subscribe((data: UserWithId[])=>{
       console.log(data);
       this.itemsA = data;
       this.ref.markForCheck();
     });
-    this.db.collection<User>('items-b').valueChanges().subscribe((data: User[])=>{
+
+    this.db.collection<User>('items-b').snapshotChanges()
+    .pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as User;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    )
+    .subscribe((data: UserWithId[])=>{
       console.log(data);
       this.itemsB = data;
       this.ref.markForCheck();
     });
+
+    this.db.collection<User>('items-c').snapshotChanges()
+    .pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as User;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    )
+    .subscribe((data: UserWithId[])=>{
+      console.log(data);
+      this.itemsC = data;
+      this.ref.markForCheck();
+    });
+
   }
 
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<UserWithId[]>) {
 
     console.log(event);
 
     if (event.previousContainer === event.container) {
       // moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
+
+
+
       transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
 
-      const previous_container_id = event.previousContainer.id;
-      console.log('previous_container_id', previous_container_id);
 
-      const current_item = event.container.data[event.currentIndex];
-      console.log('current_item', current_item);
+        console.log('event.currentIndex', event.currentIndex);
+        console.log('event.container.data', event.container.data);
+        console.log('event.container.data[0]', event.container.data[0]);
+        console.log('event.container.data[1]', event.container.data[1]);
+        console.log('event.container.data[2]', event.container.data[2]);
+        console.log('event.container.data.length', event.container.data.length);
+        const current_item: UserWithId = event.container.data[event.currentIndex];
+        console.log('current_item', current_item);
 
+        const id = '' + current_item.id;
+        delete current_item.id;
+        const item = current_item;
+        const previous_container_id = event.previousContainer.id;
+        const container_id = event.container.id;
+        console.log('previous_container_id', previous_container_id);
+
+        this.delete_from_firebase(previous_container_id, id);
+        this.add_to_firebase(container_id, current_item);
 
     }
   }
+
+  delete_from_firebase(path: string, id: string){
+    console.log(`delete_from_firebase`);
+    const itemDoc = this.db.doc<User>(`${path}/${id}`);
+    itemDoc.delete();
+  }
+
+  add_to_firebase(path: string, item: User){
+    console.log(`add_to_firebase`);
+    const collection = this.db.collection<User>(`${path}`);
+    collection.add(item);
+  }
+
 
 }
