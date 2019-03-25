17f05f5d55802437adef53d850c2ed4b2f417841
morninng <mr_morninng_star_dash@yahoo.co.jp>
2019-03-25 19:28:39 +0900

    user is inside

--- src/app/user-show/user-show.component.ts
+++ src/app/user-show/user-show.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-show',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-show.component.scss']
 })
 export class UserShowComponent implements OnInit {
+
+  @Input() user
 
   constructor() { }
 
